704204891a0bffd1e8394a3bbf96b2962f140bd9
Tobi123K <43577029+Tobi123K@users.noreply.github.com>
2018-10-02 15:16:47 +0100

    Update bot.py
    Updated the code to display a line with multiple escape characters.

--- 1-basic/1-output/3-escape-characters/bot.py
+++ 1-basic/1-output/3-escape-characters/bot.py
@@ -4,3 +4,7 @@ print("\t Display a tab space")
 print("\\ Display a back slash")
 print("\' Display a single quote")
 print("\" Display a double quote")
+
+#Display I am Beep message
+
+print ("\n\t\t\"I am Beep!\" ")
