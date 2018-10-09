35f178a3bb36c97d50bba5f034a7b61605272395
Tobi123K <43577029+Tobi123K@users.noreply.github.com>
2018-10-09 14:41:56 +0100

    Create 2-age.py
    Code to calculate  age in days

---
+++ 1-basics/6-review/2-input/2-age.py
@@ -0,0 +1,9 @@
+# Useful constatns
+DAYS_IN_YEAR = 365.25
+# Ask a user to give a age
+print(" Please, type your age in years")
+age = int(input())
+
+# Convert user's age into the days
+age_in_days = age * DAYS_IN_YEAR
+print ("You are living", age_in_days, "days!")
