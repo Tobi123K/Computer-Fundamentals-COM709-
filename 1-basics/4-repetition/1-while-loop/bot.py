e231d608e1b9a24b11091152923f8d050ee10ef2
Tobi123K <43577029+Tobi123K@users.noreply.github.com>
2018-10-09 16:16:49 +0100

    Create bot.py
    Code to create the loop

---
+++ 1-basics/4-repetition/1-while-loop/bot.py
@@ -0,0 +1,9 @@
+number_draw = int(input())
+count = 0
+while (count < number_draw):
+  print("*************")
+  print("*           *")
+  print("**0******0***")
+  print("*************")
+  print("****----*****")
+  count = count + 1
