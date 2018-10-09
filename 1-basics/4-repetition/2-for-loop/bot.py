53ce26205e4396e836db1f08c5868ffc46ebf1f8
Tobi123K <43577029+Tobi123K@users.noreply.github.com>
2018-10-09 16:20:25 +0100

    Create bot.py
    For loop

---
+++ 1-basics/4-repetition/2-for-loop/bot.py
@@ -0,0 +1,12 @@
+print("Type how many ascii robots should I draw?")
+
+number_draw = int(input())
+count = 0
+for count in range (0,number_draw, 1):
+  print("*************")
+  print("*           *")
+  print("**0******0***")
+  print("*************")
+  print("****----*****")
+  print("             ")
+  count = count + 1
