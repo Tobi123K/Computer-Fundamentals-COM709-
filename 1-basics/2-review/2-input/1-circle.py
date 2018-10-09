87a73e3aa585b079ec5a1de0849739c08494c078
Tobi123K <43577029+Tobi123K@users.noreply.github.com>
2018-10-09 14:29:59 +0100

    Create 1-circle.py

---
+++ 1-basics/2-review/2-input/1-circle.py
@@ -0,0 +1,13 @@
+import math
+
+# ask use to enter raidus
+print ("Please Enter radius")
+# read user's response
+radius =float(input())
+# calculate the are and circumference
+area = math.pi *(radius**2)
+# alternaively
+# are = math.pi*(radius)
+circumference = 2 * math.pi * radius
+print ("Are is ", round(area, 2)
+print ("sdsad", round(circumference, 2))
